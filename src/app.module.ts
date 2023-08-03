c39139acf05336f74026790567dbd04e9940366a
Rafael Mariano <rafael.silva@digi.ag>
2023-08-03 13:39:01 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,25 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ClientsModule } from './clients/clients.module';
 import { Client } from './clients/entities/client.entity';
+import { ServicesModule } from './services/services.module';
+import { Service } from './services/entities/service.entity';
+import { BudgetModule } from './budget/budget.module';
+import { Budget } from './budget/entities/budget.entity';
+import { BudgetServiceEntity } from './budget/entities/budget-service.entity';
+import { ServiceOrderModule } from './service-order/serviceOrder.module';
+import { ServiceOrder } from './service-order/entities/service-order.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Client],
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'rafa1234',
+    database: 'postgres',
+    entities: [User, Client, Service, Budget, BudgetServiceEntity, ServiceOrder],
     synchronize: true
-  }), UserModule, AuthModule, ClientsModule],
+  }), UserModule, AuthModule, ClientsModule, ServicesModule, BudgetModule, ServiceOrderModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
