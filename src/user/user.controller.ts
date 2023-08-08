b85c4c0c3fb281b256a0f65612b10d644b10bcb8
Rafael Mariano <rafael.silva@digi.ag>
2023-08-08 18:09:59 -0300

    feat: create service for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @IsPublic()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserNotExistException } from 'src/exceptions/user-not-exist.exception';
 
 @Injectable()
 export class UserService {
@@ -17,7 +19,26 @@ export class UserService {
     return this.repo.save(data);
   }
 
+  findAll() {
+    return this.repo.find();
+  }
+
   findByEmail(email: string) {
     return this.repo.findOne({ where: { email } });
+  }
+
+  async update(id: number, updateUser: UpdateUserDto): Promise<User> {
+    const user = await this.repo.findOne(id);
+    if (!user) throw new UserNotExistException();
+
+    Object.assign(user, updateUser);
+    return await this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne(id);
+    if (!user) throw new UserNotExistException();
+
+    return this.repo.remove(user);
   }
 }
